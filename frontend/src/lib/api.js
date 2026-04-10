96d60607fa80438c420671e3b674af0eee05a01a
Aanu <aladeniyiaanu@gmail.com>
2026-04-10 14:31:29 +0100

    added deployed backend url to frontend

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,6 +1,7 @@
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
-const BASE_URL = 'http://localhost:8000';
+const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000';
+
 
 export const api = {
   async register(data) {
